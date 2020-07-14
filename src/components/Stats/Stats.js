e9d2bf6d4078f2b442bc04de32d3038f31a5f7b0
Ken Cheung <wasd0109.dev@gmail.com>
2020-07-15 01:23:02 +0800

    Change stat to investigate mobile behaviour

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -13,8 +13,8 @@ function Stats({ stats }) {
           className="w-12/12 h-auto border-l-0 border-r-2 border-b-2 m-auto"
         />
         {stats.map((stat, i) => {
-          if (stat[0] === 'id' || stat[0] === 'userid' || stat[0] === 'message')
-            return null;
+          // if (stat[0] === 'id' || stat[0] === 'userid' || stat[0] === 'message')
+          //   return null;
           return stat[1] ? (
             <li
               key={stat[0]}
