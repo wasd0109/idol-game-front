e4499169608333cc409105d1cb9acf42a8d1756a
Ken Cheung <wasd0109.dev@gmail.com>
2020-08-14 18:47:11 +0800

    Add Navbar indication of current page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const generateNavBarButton = (name, link) => (
+const generateNavBarButton = (name, link, currentLocation) => (
   <Link
     key={name}
     to={link}
-    className="transition duration-500 hover:bg-white h-full pt-4 pr-2"
+    className={`transition duration-500 hover:bg-white h-full pt-4 pr-2 ${
+      currentLocation === link ? 'bg-white' : ''
+    }`}
   >
     <p className="text-lg font-medium">{name}</p>
   </Link>
 );
 
 function Navbar({ onLogout, navBarContent }) {
+  const location = useLocation();
   return (
     <nav className="flex flex-wrap bg-blue-300 pl-4" id="navbar">
       {navBarContent.map((button) =>
-        generateNavBarButton(button[0], button[1])
+        generateNavBarButton(button[0], button[1], location.pathname)
       )}
       <Link
         to="/"
@@ -25,7 +28,6 @@ function Navbar({ onLogout, navBarContent }) {
         <p className="text-lg font-medium">Logout</p>
       </Link>
     </nav>
-
   );
 }
 
