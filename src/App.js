62afe8356fbf0d342ef8bca848746909934f8f61
けん <wasd0109@Kens-MacBook-Air.local>
2021-06-26 19:41:36 +0900

    ASupport backend register API changes

--- src/App.js
+++ src/App.js
@@ -7,18 +7,19 @@ import './output.css';
 import './App.css';
 const LazyRegister = React.lazy(() => import('./components/Register'));
 
-
 function App() {
   const initialUser = {
     username: localStorage.getItem('username')
       ? localStorage.getItem('username')
       : '',
-    userID: localStorage.getItem('userID') ? localStorage.getItem('userID') : "0",
-  }
+    userID: localStorage.getItem('userID')
+      ? localStorage.getItem('userID')
+      : '0',
+  };
 
-  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('loggedIn')
-    ? localStorage.getItem('loggedIn')
-    : false);
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.getItem('loggedIn') ? localStorage.getItem('loggedIn') : false
+  );
 
   const [user, setUser] = useState(initialUser);
 
@@ -38,53 +39,64 @@ function App() {
   const [error, setError] = useState(undefined);
 
   const onSubmit = (username, password, playerName = undefined) => {
-    if ((!username || !password || playerName === null)) {
-      return setError("Please enter all information")
+    if (!username || !password || playerName === null) {
+      return setError('Please enter all information');
     }
-    const data = playerName ? { username, password, name: playerName } : { username, password };
-    const url = playerName ? 'https://idol-game.herokuapp.com/register' : 'https://idol-game.herokuapp.com/login';
+    const data = playerName
+      ? { username, password, playerName }
+      : { username, password };
+    const url = playerName
+      ? 'http://localhost:3001/register'
+      : 'https://idol-game.herokuapp.com/login';
     fetch(url, {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
-    }).then((res) => res.json()).then(data => {
-      const { username, id } = data;
-      if (username && id) {
-        setUser({ username: username, userID: id });
-        setLoggedIn(true);
-      }
-      else setError(data)
-    }).catch((error) => setError(error))
-  }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const { username, id } = data;
+        if (username && id) {
+          setUser({ username: username, userID: id });
+          setLoggedIn(true);
+        } else setError(data);
+      })
+      .catch((error) => setError(error));
+  };
 
   const resetError = () => {
     setError(null);
-  }
+  };
 
   return loggedIn ? (
     <MainPage userID={userID} setLoggedIn={setLoggedIn} />
   ) : (
-      <Suspense
-        fallback={
-          <div className="flex justify-center mt-24">
-            <Loader type="TailSpin" color="#00BFFF" height={200} width={200} />
-          </div>
-        }
-      >
-        <Router>
-          <Switch>
-            <Route path="/register">
-              <LazyRegister onSubmit={onSubmit} error={error} resetError={resetError} />
-            </Route>
-            <Route path="/">
-              <Login onSubmit={onSubmit} error={error} resetError={resetError} />
-            </Route>
-          </Switch>
-        </Router>
-      </Suspense>
-    );
+    <Suspense
+      fallback={
+        <div className="flex justify-center mt-24">
+          <Loader type="TailSpin" color="#00BFFF" height={200} width={200} />
+        </div>
+      }
+    >
+      <Router>
+        <Switch>
+          <Route path="/register">
+            <LazyRegister
+              onSubmit={onSubmit}
+              error={error}
+              resetError={resetError}
+            />
+          </Route>
+          <Route path="/">
+            <Login onSubmit={onSubmit} error={error} resetError={resetError} />
+          </Route>
+        </Switch>
+      </Router>
+    </Suspense>
+  );
 }
 
 export default App;
